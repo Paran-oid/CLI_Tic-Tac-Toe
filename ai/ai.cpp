2ac61034825d152e7a7a7886cfb3705f68310c8b
Paran-oid <azizhmid10000@gmail.com>
2024-10-30 09:07:28 +0100

    included pointers improved overall functionality improved directory structure

--- ai/ai.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by aziz on 9/14/24.
-//
-
-#include "ai.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,36 @@
 #include <algorithm>
-#include "user/user.h"
-#include "./program/output.h"
 #include <random>
 #include <chrono>
+#include <memory>
 
-#include "board/board.h"
+#include "board.h"
+#include "user.h"
+#include "output.h"
+
+
 
 int main()
 {
     bool isRunning = true;
     char userInput = ' ';
 
-    user user1;
-    user user2;
+    std::unique_ptr<user> user1;
+    std::unique_ptr<user> user2;
 
-    std::srand(std::chrono::steady_clock::now().time_since_epoch().count());
-    int beginUser = std::rand() % 2 + 1;
+    int beginUser;
+    //assign the user who will begin
+    {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution uid(1, 2);
+        beginUser = uid(mt);
+    }
 
     while(isRunning)
     {
-        user1.set_name(output::get_username(1));
-        user2.set_name(output::get_username(2));
-        int user = output::x_or_o(beginUser);
+        user1->set_name(output::get_username(1));
+        user2->set_name(output::get_username(2));
+        const int user = output::x_or_o(beginUser);
 
         std::cin >> userInput;
         while(userInput != 'X' && userInput != 'O')
@@ -32,13 +41,13 @@ int main()
 
         if(user == 1)
         {
-            user1.set_operator(std::toupper(userInput));
-            user2.set_operator(userInput == 'X' ? 'O' : 'X');
+            user1->set_operator(std::toupper(userInput));
+            user2->set_operator(userInput == 'X' ? 'O' : 'X');
         }
         else
         {
-            user2.set_operator(std::toupper(userInput));
-            user1.set_operator(userInput == 'X' ? 'O' : 'X');
+            user2->set_operator(std::toupper(userInput));
+            user1->set_operator(userInput == 'X' ? 'O' : 'X');
         }
 
         board::run_game(user1, user2);
